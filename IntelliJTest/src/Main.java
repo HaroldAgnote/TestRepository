580b4cd06354bd372177e3f6ecce12664c3ad445
Harold Agnote <haroldagnote38@hotmail.com>
2016-08-06 01:07:03 -0700

    Added IntelliJ Test Files

---
+++ IntelliJTest/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Harold on 8/6/2016.
+ */
+public class Main
+{
+    public static void main(String[]args)
+    {
+
+    }
+}
---
+++ IntelliJTest/src/TestClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Harold on 8/6/2016.
+ */
+public class TestClass
+{
+    public TestClass()
+    {
+
+    }
+}
